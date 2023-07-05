11e3f910ca120a3d8476db51f1d3583827f2f3d7
amaniesseili <esseiliamani@hotmail.com>
2023-07-05 20:42:57 +0200

    bg- log in with gioco dei dadi ex. completed

--- main1.js
+++ main1.js
@@ -13,19 +13,70 @@ let emailFound = false
 
 
 btnSubmit.addEventListener ("click", function() {
+    const userEmailElement = document.querySelector("[name='email']").value
 
     for (let i=0; i < mailingList.length; i++){
     
         const userEmailElement = mailingList[i]
         
-        if ( userEmailElement === mailingList){
+        if ( userEmailElement === mailingList[i]){
             document.getElementById("registraion-mess").innerHTML = ( "sei già registrato e puoi effettuare l'accesso");
             emailFound = true
         }else if ( userEmailElement !== mailingList[i] && emailFound === false)
         document.getElementById("registraion-mess").innerHTML = ( "Ci dispiace, ma l'accesso è limitato agli utenti registrati.");
+        emailFound = false
 
 
     }
 
 })
 
+// ----------------------- esercizio 2 -----------------
+
+// numero random per l'utente 
+var userRandomNumber =
+ Math.floor(Math.random() * 6) + 1; 
+
+document.getElementById("user-rand-num").innerHTML =  userRandomNumber;
+
+
+ // numero random per il computer
+var computerRandomNumber =
+ Math.floor(Math.random() * 6) + 1; 
+
+document.getElementById("com-rand-num").innerHTML = computerRandomNumber;
+
+
+const btnStart = document.querySelector(".btn-start");
+
+
+
+// controllare il vincitore 
+btnStart.addEventListener ("click", function() {
+
+    // var userRandomNumber =
+    // Math.floor(Math.random() * 6) + 1; 
+
+    // var computerRandomNumber =
+    // Math.floor(Math.random() * 6) + 1; 
+   
+
+    if (userRandomNumber > computerRandomNumber ){
+        document.getElementById("final.mess").innerHTML = ( "Il giocatore ha vinto ");
+    
+    }else if (userRandomNumber < computerRandomNumber){
+        document.getElementById("final.mess").innerHTML = ( "Il computer ha vinto ");
+
+    }else{
+        document.getElementById("final.mess").innerHTML = ( " E UN PAREGGIO  ");
+    }
+
+    // per nasconder il numeri random
+    document.getElementById("com-rand-num").classList.add("d-none")
+    document.getElementById("user-rand-num").classList.add("d-none")
+
+})
+
+
+
+// come faccio a far apparire i numeri random soltanto quando clicco il pulsante 
